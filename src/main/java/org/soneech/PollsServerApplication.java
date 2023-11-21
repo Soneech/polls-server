f7a8dcc8e86d2a23ea81b28c07917ccbe3e318fc
Soneech <sonya.patrina@yandex.ru>
2023-11-21 16:32:42 +0300

    Initial commit

---
+++ src/main/java/org/soneech/PollsServerApplication.java
@@ -0,0 +1,11 @@
+package org.soneech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PollsServerApplication.class, args);
+    }
+}
---
+++ src/test/java/org/soneech/PollsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soneech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
